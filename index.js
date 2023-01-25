73f83203368fe97d6459557297be3ed3c460a511
Amy Snarr <amy.snarr16@gmail.com>
2023-01-24 21:38:45 -0700

    made separate folders and files for each action

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // const sequelize = require('./config/connection');
 const inquirer = require('inquirer');
 // const fs = require('fs');
+const answerOptions = require('./options/index');
+
 
 // Starting point for questions
 const init = () => {
@@ -19,41 +21,13 @@ const init = () => {
                     'Quit'],
         }
     ])
-    // Choice response triggers another function
+    // Choice response triggers functions inside folder >options
     .then((data) => {
-        switch(data.options) {
-            case 'View all departments':
-                console.log(data.options);
-                init();
-                break;
-            case 'View all roles':
-                console.log(data.options);
-                init();
-                break;
-            case 'View all employees':
-                console.log(data.options);
-                init();
-                break;
-            case 'Add a department':
-                console.log(data.options);
-                init();
-                break;
-            case 'Add a role':
-                console.log(data.options);
-                init();
-                break;
-            case 'Add an employee':
-                console.log(data.options);
-                init();
-                break;
-            case 'Update an employee role':
-                console.log(data.options);
-                init();
-                break;
-            case 'Quit':
-                console.log(data.options);
-                init();
-                break;
+        if (data.options === 'Quit') {
+            console.log('Goodbye')
+        } else {
+            answerOptions(data);
+            init();
         }
     })
 }
